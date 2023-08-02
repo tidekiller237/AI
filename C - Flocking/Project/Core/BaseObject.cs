269d0d660a55f16df874941e5e304a077b645ffe
tidekiller237 <42755734+tidekiller237@users.noreply.github.com>
2023-08-01 22:06:06 -0500

    Uploaded third project

---
+++ C - Flocking/Project/Core/BaseObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using Microsoft.DirectX;
+
+namespace FullSailAFI.SteeringBehaviors.Core
+{
+    public class BaseObject
+    {
+        #region Properties
+
+        public Vector3 Position { get; set; }
+        public float CollisionRadius { get; set; }
+         
+        #endregion
+
+        #region Constructors
+        public BaseObject()
+        {
+            Position = Vector3.Empty;
+            CollisionRadius = 0;
+        }
+        #endregion
+    }
+}
---
+++ C - Flocking/Project/SteeringBehaviors/ApplicationPresentation/RenderControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FullSailAFI.SteeringBehaviors.ApplicationPresentation
+{
+    public partial class RenderControl : UserControl
+    {
+        public RenderControl()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.Opaque, true);
+        }
+    }
+}
